ad1951f8a7b10d66c08b3506ed67720264f08e54
ItzKashiii <150389478+ItzKashiii@users.noreply.github.com>
2023-12-03 18:21:28 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Accept user input
+name = input("Enter your name: ")
+address = input("Enter your address: ")
+gender = input("Enter your gender: ")
+program_course = input("Enter your program/course: ")
+
+
+# Display information
+print("\nInformation Entered:")
+print(f"Name: {name}")
+print(f"Address: {address}")
+print(f"Gender: {gender}")
+print(f"Program/Course: {program_course}")
